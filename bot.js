121483546b9d93ce35f26601596d70b43ae0864e
mfdt2sdsad <46102331+mfdt2sdsad@users.noreply.github.com>
2018-12-24 05:13:55 -0600

    g

--- bot.js
+++ bot.js
@@ -1,22 +1,21 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const discord_token = process.env.TOKEN;
-const prefix = process.env.PREFIX;
 
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://nricbottest.herokuapp.com/`);
-}, 280000);
-
-client.on("ready", () => {
-  console.log("I am ready!");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
- 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
-});
- 
 
-client.login(discord_token);
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.TOKEN);
