5d11ccb98783b7c962c2299377e846c2061fea3d
mfdt2sdsad <46102331+mfdt2sdsad@users.noreply.github.com>
2018-12-24 04:22:19 -0600

    g

--- bot.js
+++ bot.js
@@ -1,21 +1,53 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
+const http = require("http");
+const express = require("express");
+const app = express();//
 
-client.on('ready', () => {
-    console.log('I am ready!');
+const discord_token = process.env.TOKEN;
+const prefix = process.env.PREFIX;
+const logs = "526705329245913090";
+const modlogs = "526705339278688287";
+const version = process.env.VERSION;
+
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://nricbottest.herokuapp.com/`);
+}, 280000);
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+  if (!file.endsWith(".js")) return;
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.content.indexOf(prefix) !== 0) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+    const embed = new Discord.RichEmbed()
+      .setAuthor("Command Logger", "https://t0.rbxcdn.com/e25a771f37859b7c227944230596bae6")
+      .setColor(0x00AE86)
+      .addField("Command:", `*${command}*`)
+      .addField("Args:",`*${args}*`)
+      .addField("Server:", `${message.guild.name}`)
+      .addField("Channel:", `${message.channel.name}`)
+      .setFooter(`Version: ${version}`);
+    client.channels.get(modlogs).send({embed});
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-const client = process.env.TOKEN;
+client.login(discord_token);
